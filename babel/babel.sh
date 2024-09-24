7f044ac6d4d0e08e14cdad3481797d24978452f9
Samuel Williams <samuelhwilliams@gmail.com>
2024-09-24 21:22:00 +0100

    Add command to babel.sh to update .po files

--- babel/babel.sh
+++ babel/babel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pybabel extract -F babel.ini -k _gettext -k _ngettext -k lazy_gettext -o admin.pot --project Flask-Admin ../flask_admin
+pybabel update -i admin.pot -d ../flask_admin/translations -D admin -N
 pybabel compile -f -D admin -d ../flask_admin/translations/
 
 # docs
